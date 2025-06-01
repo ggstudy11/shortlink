12c67a9092bd9e2ff642afa15ec2a0e210a42e2d
ggstudy11 <439403849@qq.com>
2025-06-01 13:33:56 +0800

    feature : 增加redis key前缀常量

--- shortlink-admin/src/main/java/com/example/shortlink/admin/common/constant/RedisCacheConstant.java
+++ shortlink-admin/src/main/java/com/example/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -2,4 +2,5 @@ package com.example.shortlink.admin.common.constant;
 
 public class RedisCacheConstant {
     public static final String LOCK_USER_REGISTER = "short-link:lock_user_register:";
+    public static final String LOGIN_USER = "short-link:login_user:";
 }
