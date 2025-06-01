4d513593a1f155ce177f1b573030e857d8ac10ee
ggstudy11 <439403849@qq.com>
2025-06-01 10:34:38 +0800

    feature : 错误码添加

--- shortlink-admin/src/main/java/com/example/shortlink/admin/common/enums/UserErrorCode.java
+++ shortlink-admin/src/main/java/com/example/shortlink/admin/common/enums/UserErrorCode.java
@@ -4,7 +4,10 @@ import com.example.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCode implements IErrorCode {
 
-    USER_NOT_EXIST("B002001", "用户不存在");
+    USER_NOT_EXIST("B002001", "用户不存在"),
+    USER_NAME_EXIST("B002002", "用户名已存在"),
+    USER_CREATE_ERROR("B002003", "用户创建失败"),
+    USER_EXIST("B002004", "用户已存在");
 
     private final String code;
 
