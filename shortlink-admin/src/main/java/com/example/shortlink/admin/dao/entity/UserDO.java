01a4b78062acf0154aeb81ec113d09b5652fb7c7
ggstudy11 <439403849@qq.com>
2025-06-02 10:57:24 +0800

    optimize : 抽离DO公共属性

--- shortlink-admin/src/main/java/com/example/shortlink/admin/dao/entity/UserDO.java
+++ shortlink-admin/src/main/java/com/example/shortlink/admin/dao/entity/UserDO.java
@@ -1,18 +1,17 @@
 package com.example.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.EqualsAndHashCode;
 
 /**
  * 用户持久层
  */
 @TableName("t_user")
 @Data
-public class UserDO {
+@EqualsAndHashCode(callSuper=true)
+public class UserDO extends BaseDO {
 
     /**
      * id
@@ -48,23 +47,5 @@ public class UserDO {
      * 删除时间戳
      */
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 更新时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0: 未删除 1: 删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 
 }
