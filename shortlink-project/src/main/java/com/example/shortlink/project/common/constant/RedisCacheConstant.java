7eb009a4fa7215d6fb61a81e31102dd2dd74ee80
ggstudy11 <439403849@qq.com>
2025-06-08 15:34:03 +0800

    feature : 增加Redis key前缀常量

---
+++ shortlink-project/src/main/java/com/example/shortlink/project/common/constant/RedisCacheConstant.java
@@ -0,0 +1,6 @@
+package com.example.shortlink.project.common.constant;
+
+public class RedisCacheConstant {
+    public static final String SHORT_URL_PREFIX = "short-link:shortUrl:";
+    public static final String LOCK_SHORT_URL_PREFIX = "short-link:lock:shortUrl:";
+}
