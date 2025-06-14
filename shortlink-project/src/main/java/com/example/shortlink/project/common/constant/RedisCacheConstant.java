c3a2be6fc840d57de8e8d7011610e9a758d8057a
ggstudy11 <439403849@qq.com>
2025-06-14 15:50:20 +0800

    update : 新增redis-key 前缀

--- shortlink-project/src/main/java/com/example/shortlink/project/common/constant/RedisCacheConstant.java
+++ shortlink-project/src/main/java/com/example/shortlink/project/common/constant/RedisCacheConstant.java
@@ -3,4 +3,5 @@ package com.example.shortlink.project.common.constant;
 public class RedisCacheConstant {
     public static final String SHORT_URL_PREFIX = "short-link:shortUrl:";
     public static final String LOCK_SHORT_URL_PREFIX = "short-link:lock:shortUrl:";
+    public static final String FULL_SHORT_URL_UIP_PREFIX = "short-link:uip:";
 }
