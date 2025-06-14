74d89657104b092f60586dd85f4041a5022e9ebf
ggstudy11 <439403849@qq.com>
2025-06-15 00:07:50 +0800

    feature : 短链接访问监控基础数据获取

--- shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkAccessStatsMapper.java
+++ shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkAccessStatsMapper.java
@@ -2,6 +2,8 @@ package com.example.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.shortlink.project.dao.entity.ShortLinkAccessStatsDO;
+import com.example.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.example.shortlink.project.dto.resp.ShortLinkBasicStatsRespDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,4 +11,6 @@ import org.apache.ibatis.annotations.Param;
 public interface ShortLinkAccessStatsMapper extends BaseMapper<ShortLinkAccessStatsDO> {
 
     int insertOrUpdate(@Param("stats") ShortLinkAccessStatsDO stats);
+
+    ShortLinkBasicStatsRespDTO countBasicStats(@Param("param")ShortLinkStatsReqDTO param);
 }
