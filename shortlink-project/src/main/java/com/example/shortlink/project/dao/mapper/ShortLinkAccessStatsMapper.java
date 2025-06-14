b54ceee0f64ed129cd2a6e73bd0f4e11e10e5516
ggstudy11 <439403849@qq.com>
2025-06-14 17:49:29 +0800

    update : mapper注解添加

--- shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkAccessStatsMapper.java
+++ shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkAccessStatsMapper.java
@@ -2,8 +2,10 @@ package com.example.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.shortlink.project.dao.entity.ShortLinkAccessStatsDO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface ShortLinkAccessStatsMapper extends BaseMapper<ShortLinkAccessStatsDO> {
 
     int insertOrUpdate(@Param("stats") ShortLinkAccessStatsDO stats);
