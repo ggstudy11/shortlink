f03ba3be5083da696e465aec6caa61eb15bb8ae0
ggstudy11 <439403849@qq.com>
2025-06-15 22:12:24 +0800

    feature : 短链接监控设备类型开发

--- shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkDeviceStatsMapper.java
+++ shortlink-project/src/main/java/com/example/shortlink/project/dao/mapper/ShortLinkDeviceStatsMapper.java
@@ -2,11 +2,17 @@ package com.example.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.shortlink.project.dao.entity.ShortLinkDeviceStatsDO;
+import com.example.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.example.shortlink.project.dto.resp.ShortLinkStatsDeviceRespDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ShortLinkDeviceStatsMapper extends BaseMapper<ShortLinkDeviceStatsDO> {
 
     int insertOrUpdate(@Param("stats")ShortLinkDeviceStatsDO stats);
+
+    List<ShortLinkStatsDeviceRespDTO> getDeviceStats(@Param("param")ShortLinkStatsReqDTO param);
 }
